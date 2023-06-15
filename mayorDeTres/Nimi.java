25cf4d736929badecee6ef3060e4164a74b8fb1d
Ledesma Daniel <daniel.ledesma@bitlogic.io>
2023-06-14 23:26:11 -0300

    se borra un signo de admiracion

--- mayorDeTres/Nimi.java
+++ mayorDeTres/Nimi.java
@@ -1,6 +1,6 @@
 // The main method must be in a class named "Main".
 class Main {
     public static void main(String[] args) {
-        System.out.println("Hola world!!!");
+        System.out.println("Hola world!!");
     }
 }
