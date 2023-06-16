ac808bb99de48c88564c36557c4bb8730c4bece7
Martin Acosta <tinchiomdq@gmail.com>
2023-06-15 23:19:37 -0300

    se corrigió el nombre de la clase y se hizo el código más legible con los TAB

--- mayorDeTres/Nimi.java
+++ mayorDeTres/Nimi.java
@@ -1,6 +1,6 @@
 // The main method must be in a class named "Main".
 class Nimi {
     public static void main(String[] args) {
-        System.out.println("Hola world!!");
+        System.out.println("asd!!");
     }
 }
