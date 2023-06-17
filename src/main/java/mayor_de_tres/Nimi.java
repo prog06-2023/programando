cd02edc498a7570967169532c2fd87b3437ae15a
Ledesma Daniel <daniel.ledesma@bitlogic.io>
2023-06-17 02:03:16 -0300

    se hace mayor de tres

--- src/main/java/mayor_de_tres/Nimi.java
+++ src/main/java/mayor_de_tres/Nimi.java
@@ -1,7 +1,25 @@
 package mayor_de_tres;
 
-public class Nimi {
+import java.util.Scanner;
+
+class Nimi {
     public static void main(String[] args) {
-        System.out.println("Hola world!!");
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Ingrese primer número");
+        int numeroUno = teclado.nextInt();
+        System.out.println("Ingrese segundo número");
+        int numeroDos = teclado.nextInt();
+        System.out.println("Ingrese tercer número");
+        int numeroTres = teclado.nextInt();
+
+        if (numeroUno > numeroDos && numeroUno > numeroTres){
+            System.out.println("El número mayor es: " + numeroUno);
+        }else if (numeroDos > numeroTres){
+            System.out.println("El número mayor es: " + numeroDos);
+        }else{
+            System.out.println("El número mayor es: " + numeroTres);
+        }
+
+        teclado.close();
     }
 }
