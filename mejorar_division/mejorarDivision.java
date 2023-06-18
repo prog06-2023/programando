775b331243da2c660b7704f62f609b69c0b0e5a0
Javier <schjavier@gmail.com>
2023-06-18 09:08:07 -0300

    mejora de division realizada.

---
+++ mejorar_division/mejorarDivision.java
@@ -0,0 +1,41 @@
+// Autor: Javier
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class DivisionMejorada {
+    public static void main(String[] args) {
+       
+            Scanner scanner = new Scanner(System.in);
+            int miNumero;
+            int divisor;
+            int dividendo;
+            int resto = 0;
+            int cociente = 0;
+            boolean seguir = true;
+           
+            while (seguir) {
+            try {
+                seguir = false;
+                System.out.println("Por favor ingrese el nro. a dividir: ");
+                miNumero = scanner.nextInt();
+                dividendo = miNumero;
+                System.out.println("Por Favor ingrese el nro. divisor: ");
+                divisor = scanner.nextInt();
+
+                while (dividendo >= divisor) {
+                    dividendo = dividendo - divisor;
+                    resto = dividendo;
+                    cociente++;
+                    }
+                System.out.println("El resultado de " + miNumero + " dividido " + divisor + " es: " + cociente + " y su resto es: " + resto);
+                
+                }
+            catch (InputMismatchException exception){
+                System.out.println("Se deben ingresar SOLO nros. enteros! Intentelo nuevamente:");
+                scanner.next();
+                seguir = true;
+            }
+            }
+        }
+}
