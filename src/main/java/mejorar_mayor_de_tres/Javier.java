0543ea6f4cd27b8eba5a7e4f71488861819a55f6
Javier <schjavier@gmail.com>
2023-06-20 08:04:43 -0300

    agrego archivo de mejorarMayorDeTres

---
+++ src/main/java/mejorar_mayor_de_tres/Javier.java
@@ -0,0 +1,29 @@
+package mejorar_mayor_de_tres;
+
+import java.util.Scanner;
+
+public class Javier {
+
+    public static void main(String[] args) {
+
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Cuantos numeros vamos a comparar?: ");
+        int nroDeVeces = teclado.nextInt();
+        int iteraciones = 0;
+        int mayor = 0;
+
+        while (iteraciones < nroDeVeces) {
+            System.out.println("Agregue un numero para comparar: ");
+            int nroIngresado = teclado.nextInt();
+
+            if (nroIngresado > mayor) {
+                mayor = nroIngresado;
+            }
+
+            iteraciones++;
+
+        }
+        System.out.println("El mayor de todos los nros. ingresados es: " + mayor);
+
+    }
+}
