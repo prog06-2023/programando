98443e8bfc7375ab9e0e3b1cdb95882d0b1568fb
Edgar <edgar.m.tuyarot@hotmail.com>
2023-06-22 13:27:33 -0300

    se agrega solucion al reto 2

--- src/main/java/mayor_de_tres/EdgarT.java
+++ src/main/java/mayor_de_tres/EdgarT.java
@@ -1,2 +1,33 @@
-package mayor_de_tres;public class EdgarT {
+package mayor_de_tres;
+
+import java.util.Scanner;
+
+public class EdgarT {
+    public static void main(String[] args) {
+        int num1, num2, num3, mayor;
+
+
+        Scanner lector = new Scanner(System.in);
+        System.out.println("ingrese 3 numeros");
+        num1 = lector.nextInt();
+        num2 = lector.nextInt();
+        num3 = lector.nextInt();
+
+        if (num1>num2){
+            mayor = num1;
+        }else if (num2>num3){
+            mayor = num2;
+        }else{
+            mayor =  num3;
+        }
+
+        if ((num3==num1) & (num2==num3)){
+            System.out.println("Todos los numeros son iguales");
+        }else{
+            System.out.println("mayor = " + mayor);
+        }
+
+
+
+    }
 }
