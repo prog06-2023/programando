c3df4cd6d640548bbe2ded954d91c151a8f18d73
Edgar <edgar.m.tuyarot@hotmail.com>
2023-06-22 13:01:33 -0300

    se agrega solucion al reto 1

---
+++ src/main/java/division/EdgarT.java
@@ -0,0 +1,13 @@
+
+package division;
+
+public class EdgarT {
+    public static void main(String[] args) {
+        double dividendo = 8;
+        double resultado;
+
+        resultado = dividendo * 0.5;
+
+        System.out.println("resultado = " + resultado);
+    }
+}
