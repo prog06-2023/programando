de500d867aeaeb769b8b2651344d6e71b54988a2
Ledesma Daniel <daniel.ledesma@bitlogic.io>
2023-06-26 20:46:01 -0300

    estructura reto

---
+++ src/main/java/una_persona/nimi/Main.java
@@ -0,0 +1,7 @@
+package una_persona.nimi;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("un mensaje");
+    }
+}
---
+++ src/main/java/una_persona/no_editar/Main.java
@@ -0,0 +1,7 @@
+package una_persona.no_editar;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("No editar");
+    }
+}
