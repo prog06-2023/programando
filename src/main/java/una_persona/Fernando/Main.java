7be6770631406d621558b700eb5ca123af1546d8
Fernando <fernando.fg392@gmail.com>
2023-07-10 17:33:01 -0600

    Mejorando parte de instruccion de ingreso de datos

--- src/main/java/una_persona/Fernando/Main.java
+++ src/main/java/una_persona/Fernando/Main.java
@@ -6,14 +6,14 @@ import java.util.Scanner;
 /**
  * Clase principal
  *
- * @version 1.0
+ * @version 1.1
  * @date 09/07/2023
  */
-public class main extends Persona{
+public class Main extends Persona{
     public static void main(String[] args){
         String[] datos = new String[3];
 
-        System.out.println("Ingrese sus datos: nombre,  fecha de nacimiento (dd/MM/yyyy)");
+        System.out.println("Ingrese sus datos: nombre,ocupacion y fecha de nacimiento (dd/MM/yyyy)");
         for (int i = 0; i < 3; i++){
             Scanner entrada = new Scanner(System.in);
             datos[i] = entrada.next();
