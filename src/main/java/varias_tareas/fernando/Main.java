ce46527ebb8cc96a5bfed8616963f4e65967c9d8
Fernando <fernando.fg392@gmail.com>
2023-07-28 18:48:04 -0600

    se pasan todos los sout de la clase tarea y mejora logica

--- src/main/java/varias_tareas/fernando/Main.java
+++ src/main/java/varias_tareas/fernando/Main.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 /**
  * Clase principal
  *
- * @version 1.1
+ * @version 1.2
  * @author Fernando García Cano
  *
  */
@@ -16,6 +16,8 @@ public class Main {
 
     public static void main(String[] args) {
         String input = "";
+        String  valor;
+        int idEncontrado;
         Scanner entrada = new Scanner(System.in);
         Persona persona = new Persona("Fernando", "García");
 
@@ -25,18 +27,71 @@ public class Main {
             input = input.toUpperCase();
             switch (input){
                 case "A":
-                    Tarea.agregarTarea(entrada, db);
+                    String[] datosTarea = new String[3];
+                    System.out.println("==============================================");
+                    System.out.println("Ingrese el nombre de la tarea: ");
+                    datosTarea[0] = entrada.next();
+                    System.out.println("Ingrese descripcion de la tarea: ");
+                    datosTarea[1] = entrada.next();
+                    datosTarea[2] = String.valueOf(Tarea.getId());
+                    System.out.println("==============================================");
+                    Tarea tarea = new Tarea(datosTarea[0], datosTarea[1], datosTarea[2]);
+                    Tarea.agregarTarea(tarea, db);
                     break;
                 case "M":
-                    Tarea.modificarTarea(entrada, db);
+                    System.out.println("==============================================");
+                    System.out.println("Ingrese el id de la tarea a modificar: ");
+                    valor = entrada.next();
+                    if(!Tarea.validarId(valor)){
+                        do {
+                            System.out.println("Valor no aceptado");
+                            valor = entrada.next();
+
+                        } while (!Tarea.validarId(valor));
+                    }
+                    idEncontrado = Tarea.encontraId(db, valor);
+                    if (idEncontrado == -1){
+                        do {
+                            System.out.println("Id de tarea no encontrado porfavor digite un id valido");
+                            valor = entrada.next();
+                            idEncontrado = Tarea.encontraId(db, valor);
+
+                        } while (idEncontrado == -1 && !Tarea.validarId(valor));
+                    }
+                    System.out.println("Ingrese el nombre de la tarea: ");
+                    db.get(idEncontrado).setNombreTarea(entrada.next());
+                    System.out.println("Ingrese descripcion de la tarea: ");
+                    db.get(idEncontrado).setDescripcion(entrada.next());
+                    Tarea.mostrarTareasActuales(db);
+
                     break;
                 case "D":
-                    Tarea.eliminarTarea(entrada, db);
+                    System.out.println("==============================================");
+                    System.out.println("Ingrese el id de la tarea a eliminar: ");
+                    valor = entrada.next();
+                    if(!Tarea.validarId(valor)){
+                        do {
+                            System.out.println("Valor no aceptado");
+                            valor = entrada.next();
+
+                        } while (!Tarea.validarId(valor));
+                    }
+                    idEncontrado = Tarea.encontraId(db, valor);
+                    if (idEncontrado == -1){
+                        do {
+                            System.out.println("Id de tarea no encontrado porfavor digite un id valido");
+                            valor = entrada.next();
+                            idEncontrado = Tarea.encontraId(db, valor);
+
+                        } while (idEncontrado == -1 && !Tarea.validarId(valor));
+                    }
+                    Tarea.eliminarTarea(idEncontrado, db);
                     break;
                 case "Q":
 
                     System.out.println(persona.getNombre() + " " + persona.getApellido() + " " + "tareas actuales: ");
                     Tarea.mostrarTareasActuales(db);
+                    entrada.close();
                     break;
                 default:
                     System.out.println("Elija una opcion valida");
