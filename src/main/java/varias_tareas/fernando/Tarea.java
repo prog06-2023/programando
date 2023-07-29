2ef13dc87667b1a0f766b92647e45ec40027e8d7
Fernando <fernando.fg392@gmail.com>
2023-07-28 18:49:24 -0600

    se mejora logica de programacion acorde a principios de responsabilidad unica y se quitan Scanner como parametro de los metodos asi como sout

--- src/main/java/varias_tareas/fernando/Tarea.java
+++ src/main/java/varias_tareas/fernando/Tarea.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 
 /**
  * Esta clase sirve para instanciar a un objeto de tipo Tarea
- * @version 1.2
+ * @version 1.3
  * @author Fernando García Cano
  */
 
@@ -45,79 +45,35 @@ public class Tarea {
 
     /**
      * Este metodo sirve para agregar una nueva tarea a nuestra base de datos de nuestro usuario
-     * @param entrada
+     * @param tarea
      * @param db
      */
-    public static void agregarTarea(Scanner entrada, ArrayList<Tarea> db){
-        String[] datosTarea = new String[3];
-        System.out.println("==============================================");
-        System.out.println("Ingrese el nombre de la tarea: ");
-        datosTarea[0] = entrada.next();
-        System.out.println("Ingrese descripcion de la tarea: ");
-        datosTarea[1] = entrada.next();
-        datosTarea[2] = String.valueOf(Tarea.id);
-        System.out.println("==============================================");
-        Tarea tarea = new Tarea(datosTarea[0], datosTarea[1], datosTarea[2]);
+    public static void agregarTarea(Tarea tarea, ArrayList<Tarea> db){
         db.add(tarea);
         mostrarTareasActuales(db);
     }
 
-    /**
-     * Este metodo nos ayuda a modificar alguna de nuestras tareas mediante el id de la tarea
-     * @param entrada
-     * @param db
-     */
-    public static void modificarTarea(Scanner entrada, ArrayList<Tarea> db){
-        String  valor;
-        int idEncontrado;
-        System.out.println("==============================================");
-        System.out.println("Ingrese el id de la tarea a modificar: ");
-        valor = entrada.next();
-        valor = validarId(valor, entrada);
-        idEncontrado = encontraId(db, valor);
-        validarIdDentroRango(idEncontrado, valor, entrada, db);
-        System.out.println("Ingrese el nombre de la tarea: ");
-        db.get(idEncontrado).setNombreTarea(entrada.next());
-        System.out.println("Ingrese descripcion de la tarea: ");
-        db.get(idEncontrado).setDescripcion(entrada.next());
-        mostrarTareasActuales(db);
-    }
 
     /**
      * Este metodo nos ayuda a eliminar alguna tarea del usuario mediante el id de la tarea
-     * @param entrada
+     * @param idEncontrado
      * @param db
      */
 
-    public static void eliminarTarea(Scanner entrada, ArrayList<Tarea> db){
-        String valor;
-        int idEncontrado;
-        System.out.println("==============================================");
-        System.out.println("Ingrese el id de la tarea a eliminar: ");
-        valor = entrada.next();
-        valor = validarId(valor, entrada);
-        idEncontrado = encontraId(db, valor);
-        validarIdDentroRango(idEncontrado, valor, entrada, db);
+    public static void eliminarTarea(int idEncontrado, ArrayList<Tarea> db){
         db.remove(idEncontrado);
         mostrarTareasActuales(db);
     }
 
     /**
      * Este metodo nos ayuda avalidar que la entrada del usuario solo sean numeros
+     *
      * @param valor
-     * @param entrada
      * @return
      */
-    public static String validarId(String valor, Scanner entrada){
+    public static boolean validarId(String valor){
         String validar = "^[0-9]*";
-        if(!valor.matches(validar)){
-            do {
-                System.out.println("Valor no aceptado");
-                valor = entrada.next();
-
-            } while (!valor.matches(validar));
-        }
-        return valor;
+        return valor.matches(validar);
     }
 
     /**
@@ -150,30 +106,6 @@ public class Tarea {
         return idEncontrado;
     }
 
-    /**
-     * Este metodo nos ayuda a asegurar que el id ingresado por el usuario este dentro de nuestra base de datos de tareas
-     * de otro modo solicitara un valido
-     * @param idEncontrado
-     * @param valor
-     * @param entrada
-     * @param db
-     * @return
-     */
-    public static int validarIdDentroRango(int idEncontrado, String valor, Scanner entrada, ArrayList<Tarea> db){
-        if (idEncontrado == -1){
-            do {
-                System.out.println("Id de tarea no encontrado porfavor digite un id valido");
-                valor = entrada.next();
-                valor = validarId(valor, entrada);
-                idEncontrado = encontraId(db, valor);
-
-            } while (idEncontrado == -1);
-            return idEncontrado;
-        }else {
-            return idEncontrado;
-        }
-    }
-
     public void setDescripcion(String descripcion) {
         this.descripcion = descripcion;
     }
@@ -192,5 +124,9 @@ public class Tarea {
 
     public String getIdTarea() {
         return idTarea;
+    }
+
+    public static int getId() {
+        return id;
     }
 }
