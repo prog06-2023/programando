0a9c30127aa1053439adc87474ab47da653a47d9
Xavpunkxd <jon20xavi@gmail.com>
2023-07-31 18:02:38 -0600

    corriguiendo el proyecto varias_tareas

--- src/main/java/varias_tareas/xavpunkxd/Main.java
+++ src/main/java/varias_tareas/xavpunkxd/Main.java
@@ -4,10 +4,5 @@
 package varias_tareas.xavpunkxd;
 
 public class Main {
-    public static void main(String[] args) {
-      Persona person1 = new Persona();
-
-      person1.AskName();
-
-    }
-}
+    
+} 
--- src/main/java/varias_tareas/xavpunkxd/Tarea.java
+++ src/main/java/varias_tareas/xavpunkxd/Tarea.java
@@ -1,34 +1,5 @@
 package varias_tareas.xavpunkxd;
 
 public class Tarea {
-    private int id;
-    private String description;
-
-    //constructor with parametters
-    public Tarea(int id, String description){
-        this.id=id;
-        this.description=description;
-    }
-
-    //constructor without parametters
-     public Tarea(){
-        this.id=0;
-        this.description=" ";
-    } 
-
-    //getter and setter for id
-    public long getId() {
-        return id;
-    }
-    public void setId(int id) {
-        this.id = id;
-    }
     
-    //getter and setter for description
-    public String getDescription() {
-        return description;
-    }
-    public void setDescription(String description) {
-        this.description = description;
-    }
 }
